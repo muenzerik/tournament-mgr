33b99c2e7942e275ef2c4e46d76f961b953f7694
Erik Muenz <muenzerik@gmail.com>
2021-03-20 00:16:53 +0100

    Simple layout for men's games
     -add some pages we imagine to have in place later

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,19 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/disciplines')
+def disciplines():
+    return render_template('disciplines.html')
+
+@app.route('/time_schedule')
+def time_schedule():
+    return render_template('time_schedule.html')
+
+@app.route('/image_gallery')
+def image_gallery():
+    return render_template('image_gallery.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
